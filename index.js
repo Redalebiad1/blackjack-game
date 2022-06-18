403e5332d7f5d1b94ead37c55d5e5189c251f648
Redalebiad1 <Lebiad.contact@gmail.com>
2022-06-18 04:52:54 +0100

    BlackJack

--- index.js
+++ index.js
@@ -69,13 +69,4 @@ function newCard(){
 
 
 
-let hands = ["rock","paper","scissor"]
-function getHands(){
-    let randomIndex = Math.floor (Math.random() *3)
-    return hands[randomIndex]
-    
-}
-console.log(getHands())
-
-
 
